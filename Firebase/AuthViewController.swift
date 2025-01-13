103755022e6cf6af78c0c302d6e5d0d3e739f317
CReyes <cd.reyes@icloud.com>
2025-01-13 17:53:13 +0100

    init Commit

---
+++ Firebase/AuthViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthViewController.swift
+//  Firebase
+//
+//  Created by Tardes on 13/1/25.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Auth"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
